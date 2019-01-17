e62fe9e2d22296fa98e0e156b0a4a771a8627ed0
zhaoshengqi <zhaoshengqi@meituan.com>
2019-01-17 18:23:36 +0800

    form index

--- src/main/java/com/ultron/admin/edu/domain/SpecialtyDO.java
+++ src/main/java/com/ultron/admin/edu/domain/SpecialtyDO.java
@@ -12,7 +12,7 @@ import java.util.Date;
  * 
  * @author zhaoshengqi
  * @email sench.zhao@gmail.com
- * @date 2018-12-10 17:27:33
+ * @date 2019-01-17 18:12:57
  */
 @Data
 public class SpecialtyDO implements Serializable {
@@ -22,24 +22,24 @@ public class SpecialtyDO implements Serializable {
 	private Long id;
 	//学校ID
 	private Long universityId;
-	//学校(冗余)
-	private String universityName;
 	//专业名称
 	private String name;
-	//学历
+	//学历 edu_qualification
 	private String qualification;
 	//学制
 	private String academicYear;
-	//专业类型
+	//专业类型 edu_specialty_type
 	private String type;
 	//专业介绍
 	private String introduction;
 	//备注
 	private String remark;
-	//状态
+	//状态 1有效 -1删除
 	private Integer status;
 	//创建时间
 	private Date createTime;
 	//修改时间
 	private Date updateTime;
+	//推荐权重（越大排序越前）
+	private Integer proposalWeight;
 }
